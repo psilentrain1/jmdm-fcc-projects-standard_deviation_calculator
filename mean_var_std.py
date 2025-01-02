d07a0b0d45c0c2da78e37f2e04d45d942418b011
James Drake <github@jamesmdrakemedia.com>
2025-01-02 21:27:15 +0000

    Initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,42 @@
 import numpy as np
 
 def calculate(list):
+    if (len(list) < 9):
+        raise ValueError("List must contain nine numbers.")
 
+    array = np.array(list).reshape(3, 3)
 
+    mean0 = np.mean(array, axis=0).tolist()
+    mean1 = np.mean(array, axis=1).tolist()
+    meanFlat = np.mean(array)
 
+    var0 = np.var(array, axis=0).tolist()
+    var1 = np.var(array, axis=1).tolist()
+    varFlat = np.var(array)
+    
+    std0 = np.std(array, axis=0).tolist()
+    std1 = np.std(array, axis=1).tolist()
+    stdFlat = np.std(array)
+
+    max0 = np.max(array, axis=0).tolist()
+    max1 = np.max(array, axis=1).tolist()
+    maxFlat = np.max(array)
+
+    min0 = np.min(array, axis=0).tolist()
+    min1 = np.min(array, axis=1).tolist()
+    minFlat = np.min(array)
+
+    sum0 = np.sum(array, axis=0).tolist()
+    sum1 = np.sum(array, axis=1).tolist()
+    sumFlat = np.sum(array)
+
+    calculations = {
+        'mean': [mean0, mean1, meanFlat],
+        'variance': [var0, var1, varFlat],
+        'standard deviation': [std0, std1, stdFlat],
+        'max': [max0, max1, maxFlat],
+        'min': [min0, min1, minFlat],
+        'sum': [sum0, sum1, sumFlat]
+    }
 
     return calculations
